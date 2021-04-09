b3ae3fdbc9b74776cc5c310640d72984fcaa3e6d
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-08 22:38:03 -0600

    creating a new portfolio functionality working

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,25 @@
 class PortfoliosController < ApplicationController
+  include UsersHelper
+
   def index
+    @portfolios = current_user.portfolios
   end
+
+  def create
+    @portfolio = Portfolio.new(name: portfolio_params)
+    @portfolio.user_id = current_user.id
+    if @portfolio.save
+      # Display success message
+      redirect_to portfolios_path
+    else
+      # Display error message
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.fetch(:name)
+  end
+
 end
