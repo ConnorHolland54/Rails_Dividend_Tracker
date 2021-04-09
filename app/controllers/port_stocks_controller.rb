1e3fcf6d23315216e71e24d7486e29bce8059fec
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-09 16:38:15 -0600

    Fixed error with adding stocks to portfolios

--- app/controllers/port_stocks_controller.rb
+++ app/controllers/port_stocks_controller.rb
@@ -1,11 +1,22 @@
 class PortStocksController < ApplicationController
   include UsersHelper
   def create
+    error = false
+      tickers = []
     port_stock_params.first.each do |id|
       if !current_user.portfolios.find(port_stock_params[1]).stocks.include?(Stock.find(id))
         port_stock = PortStock.create(portfolio_id: port_stock_params[1].to_i, stock_id: id.to_i, shares: 0)
       else
-        flash[:notice] = "Stock already exists in portfolio"
+        error = true
+        tickers << id
+      end
+      if error
+        symbols = []
+        tickers.each do |id|
+          symbols << Stock.find(id.to_i).symbol
+        end
+        message = "Stock(s) already exists: #{symbols.join(', ')}"
+        flash[:notice] = message
       end
     end
     redirect_to portfolio_stocks_path(port_stock_params[1].to_i)
